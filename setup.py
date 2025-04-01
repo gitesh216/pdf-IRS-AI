badd2ac7063314940105b05392c239648a0052f0
gitesh216 <109015479+gitesh216@users.noreply.github.com>
2025-04-02 00:19:10 +0530

    File Structure Update

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'IRS Generative AI Project',
+    version= '0.0.0',
+    author= 'Gitesh',
+    author_email= 'giteshzope19@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
